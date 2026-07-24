c0271bd2da32935afeb515a83377a12ec0cea5d4
okosaleo <okosaleonel@gmail.com>
2026-07-24 16:44:45 +0100

    again

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,4 +2,10 @@
 import { auth } from "@/app/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+
+
 export const { POST, GET } = toNextJsHandler(auth);
