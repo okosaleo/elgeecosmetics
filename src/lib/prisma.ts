7b8588569bbeac843dafa16ef9d72933bce93b85
okosaleo <okosaleonel@gmail.com>
2026-08-04 15:40:38 +0100

    used neon adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,19 @@
-
-// Import the driver adapter for your specific database (example uses PostgreSQL)
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// Initialize the adapter according to your driver's requirements
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
-// Pass the adapter instance to PrismaClient
-export const prisma = new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
