24e928b82ee90ac1159db6ba837df66a4011aff7
okosaleo <okosaleonel@gmail.com>
2026-08-04 14:04:22 +0100

    finished

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactCompiler: true,
-  outputFileTracingIncludes: {
-    '/**': [
-      './node_modules/pg/**',
-      './node_modules/pg-types/**',
-      './node_modules/pg-connection-string/**',
-      './node_modules/pg-protocol/**',
-      './node_modules/pg-int8/**',
-      './node_modules/postgres-array/**',
-      './node_modules/postgres-bytea/**',
-      './node_modules/postgres-date/**',
-      './node_modules/postgres-interval/**',
-    ],
-  },
   images: {
     unoptimized: true,
     qualities: [75, 100],
